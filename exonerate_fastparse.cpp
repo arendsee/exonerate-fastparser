2d9c34537705bf0388ab0b4e73c87b7557b746f6
Zebulun Arendsee <arendsee@iastate.edu>
2015-08-12 16:35:15 -0500

    report position of first stop codon rather than boolean presence

--- exonerate_fastparse.cpp
+++ exonerate_fastparse.cpp
@@ -14,19 +14,38 @@
 #include <iostream>
 using namespace std;
 
-void parse_vulgar_simple( string line, bool stop, char delim);
-void parse_vulgar_fulltab(string line, bool stop, char delim);
-void parse_vulgar_verbose(string line, bool stop, char delim);
+void parse_vulgar_simple( string line, int stop, char delim);
+void parse_vulgar_fulltab(string line, int stop, char delim);
+void parse_vulgar_verbose(string line, int stop, char delim);
+int get_first_integer(string line);
+int get_stop_position(string line, int offset);
 void write_fulltab_header();
 
 int main()
 {
     write_fulltab_header();
+
+    // buffer to hold data input
     char buffer[BUFFER_SIZE];
+
+    // string to hold line input
     string line;
+
+    // index for ordering within a record
     int position = 0;
+
+    // flag for being within the alignment region of a record
     bool between = true;
-    bool stop = false;
+
+    // query start position for current alignment segment
+    int running_position = 0;
+
+    // position of first stop codon, 0 indicates none found
+    int stop = 0;
+
+    // temporary storage for output of get_stop_position
+    int tmp_stop;
+
     while(fgets(buffer, BUFFER_SIZE, stdin)){
 
         // cast the cstring as a string object
@@ -44,14 +63,44 @@ int main()
             case 2: if(line[0] == 'v'){
                         parse_vulgar_fulltab(line, stop, '\t');
                         between = true;
-                        stop = false;
+                        stop = 0;
+                    } else {
+                        running_position = get_first_integer(line);
                     }
-            case 4: if(line.find('*') != size_t(-1))
-                        stop = true;
+            case 4: if(stop == 0) {
+                        tmp_stop = get_stop_position(line, running_position);
+                        if(tmp_stop != 0 && stop == 0){
+                            stop = tmp_stop; 
+                        }
+                    }
         }
 
         position++;
     }
+}
+
+int get_stop_position(string line, int pos){
+    for(unsigned int i = 0; i < line.length(); i++){
+        // All capital letters signal a new amino acid
+        if(line[i] >= 'A' && line[i] <= 'Z'){
+            pos++;            
+        }
+        // I am interested only in the position of the first stop,
+        // so return when first '*' is encountered
+        else if(line[i] == '*'){
+            return(pos);
+        }
+    }
+    // Return 0 if no stop is found
+    return 0;
+}
+
+int get_first_integer(string line){
+    int x;
+    std::stringstream ss;
+    ss << line;
+    ss >> x;
+    return x;
 }
 
 void write_fulltab_header(){
@@ -64,7 +113,7 @@ void write_fulltab_header(){
          << "target_stop\t"
          << "target_strand\t"
          << "score\t"
-         << "has_nonsense\t"
+         << "first_stop\t"
          << "has_frameshift\t"
          << "num_split_codons\t"
          << "num_intron\t"
@@ -72,7 +121,7 @@ void write_fulltab_header(){
          << endl;
 }
 
-void parse_vulgar_fulltab(string line, bool stop, char delim){
+void parse_vulgar_fulltab(string line, int stop, char delim){
     stringstream s(line);
     string word;
     for(int i = 0; i < 10 && s >> word; i++){
